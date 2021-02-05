712e5f0bcbe8024fb40593792d21b458af43af9d
Charlie Vo <charlievo22@gmail.com>
2021-02-05 15:30:23 +0100

    Initial commit

---
+++ src/test/java/com/example/webprogrammering/WebprogrammeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webprogrammering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprogrammeringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
